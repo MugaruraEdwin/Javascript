4a8262a07cb7b21355d6520b17d7c1754fe8d9ea
MugaruraEdwin <mugarura13@gmail.com>
2023-06-20 12:44:23 +0300

    Second commit

---
+++ helloWorld.js
@@ -0,0 +1,54 @@
+console.log("Hello World")
+
+// 1. Comments
+// used to provide information about the code 
+// can be used in general to provide any information or suggestions
+// makes it easy to understand the code or get more information about the code
+// can also be used to comment out code sections
+// ignored by javascript engine
+// can be single lined or multilined 
+// examples 
+// This is a single line comment
+/*
+This is a multiline comment
+*/
+
+// 2. Variables
+/*
+Used to store data
+can be looked at as containers for storing data values
+must contain only letters, digits, or $ and _ symbol.
+*/
+// 2.1 Guidelines for Naming Variables
+/*
+Must beigin with a letter/ underscore
+Must not start with a number
+Must not be  a reserved key word like if, else, boolean, let, var, const etc.
+Variables are case sensitive
+Use meaningful names
+Use camelCase notation (begin with lowercase then uppercase )
+// var was used in earlier versions but now we use let and const
+*/
+// 2.2 Examples
+//Variable declaration
+
+let age;
+
+// Variable initialization
+
+age = 23;
+
+//  = this is called an assignment operator
+
+// Otherway
+let studentName = "Pamela";
+console.log(studentName);
+
+// Declaring many variables on the same line
+
+let firstName, lastName, cohort, year;
+firstName = "John";
+lastName = "Doe";
+cohort = 4;
+year = 2023;
+
